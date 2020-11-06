2b710f238e1383d6b5b012c5e5bc693f8752dffe
sujith kumar <senthilnathan.subash@colanonline.com>
2020-11-06 09:17:35 +0530

    Updates

---
+++ precache-manifest.ca2f14cc809869860fa5e604ea796aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cb52f7f7202bec6a53ae06218a45bf",
+    "url": "/amsreact/index.html"
+  },
+  {
+    "revision": "549223b56180713ed1d2",
+    "url": "/amsreact/static/css/main.c1b04a70.chunk.css"
+  },
+  {
+    "revision": "0dc958d5c48ecfba2470",
+    "url": "/amsreact/static/js/2.97243703.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/amsreact/static/js/2.97243703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549223b56180713ed1d2",
+    "url": "/amsreact/static/js/main.e2f8fa33.chunk.js"
+  },
+  {
+    "revision": "78aaf608a5652441607e",
+    "url": "/amsreact/static/js/runtime-main.ff440ce9.js"
+  }
+]);
